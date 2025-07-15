85e6ebc9a7117921b63cb88f53e8d418c72280e3
Shinya <rikutoweb5@gmail.com>
2025-07-15 20:10:44 +0900

    Revox作成_mainおよびdevelopを作成

---
+++ src/main/java/com/rikuto/revox/RevoxApplication.java
@@ -0,0 +1,13 @@
+package com.rikuto.revox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevoxApplication.class, args);
+	}
+
+}
