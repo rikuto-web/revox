33ffe84e0fe14e04e9d60a7ee228e7dbefd56855
Shinya <rikutoweb5@gmail.com>
2025-08-08 21:30:05 +0900

    Imageインタフェースの実装

---
+++ src/main/java/com/rikuto/revox/service/ImageUploadService.java
@@ -0,0 +1,25 @@
+package com.rikuto.revox.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 使用クラウドに依存せず画像の操作を行うためのインターフェースです。
+ */
+public interface ImageUploadService {
+
+	/**
+	 * 画像ファイルのアップロードを行います。
+	 *
+	 * @param file 画像ファイル
+	 * @param folder 保存場所
+	 * @return アップロードが成功した画像の公開URL
+	 */
+	String uploadImage(MultipartFile file, String folder);
+
+	/**
+	 * 画像ファイルの削除を行います。
+	 *
+	 * @param imageUrl 削除したい画像のURL
+	 */
+	void deleteImage(String imageUrl);
+}
