a569d52a8c4f2ac671cecf8023032412f0356f2e
Shinya <rikutoweb5@gmail.com>
2025-08-25 12:40:30 +0900

    CICDテスト

--- update-deploy.sh
+++ update-deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
-# 最新イメージを取得
-docker pull us-west1-docker.pkg.dev/revoxprod/revox-repository/backend:latest
+# Artifact Registryの認証情報を設定
+gcloud auth configure-docker us-west1-docker.pkg.dev --quiet
+
+# revoxディレクトリが存在しない場合に作成
+mkdir -p /home/rikutoweb5_gmail_com/revox
+
+# Artifact Registryから最新のイメージをプル
+sudo docker pull us-west1-docker.pkg.dev/revoxprod/revox-repository/backend:latest
 
 # 古いコンテナを停止して削除
-docker-compose -f /home/rikutoweb5/revox/docker-compose-prod.yml down
+sudo docker-compose -f /home/rikutoweb5_gmail_com/revox/docker-compose-prod.yml down
 
 # 新しいコンテナを起動
-docker-compose -f /home/rikutoweb5/revox/docker-compose-prod.yml up -d
+sudo docker-compose -f /home/rikutoweb5_gmail_com/revox/docker-compose-prod.yml up -d
