21692528213c3c177648866bedcac36a0beec360
Shinya <rikutoweb5@gmail.com>
2025-08-25 14:12:31 +0900

    CICDテスト

--- update-deploy.sh
+++ update-deploy.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
 
-# Artifact Registryの認証情報を設定
+# Secret Manager から env を取得
+gcloud secrets versions access latest --secret="revox-prod-env" --project=revoxprod > /home/rikutoweb5/revox/secret/prod.env
+
+# Vertex AI 用 JSON
+gcloud secrets versions access latest --secret="prod-vertexAI-sa-json" --project=revoxprod > /home/rikutoweb5/revox/secret/prod-vertexAI-sa.json
+
+# Google credential JSON
+gcloud secrets versions access latest --secret="google-credential-prod-json" --project=revoxprod > /home/rikutoweb5/revox/secret/google-credential.prod.json
+
+# Artifact Registry 認証
 gcloud auth configure-docker us-west1-docker.pkg.dev --quiet
 
 # revoxディレクトリが存在しない場合に作成
-mkdir -p /home/rikutoweb5_gmail_com/revox
+mkdir -p /home/rikutoweb5/revox
 
-# Artifact Registryから最新のイメージをプル
+# 最新イメージをPull
 sudo docker pull us-west1-docker.pkg.dev/revoxprod/revox-repository/backend:latest
 
 # 古いコンテナを停止して削除
-sudo docker-compose -f /home/rikutoweb5_gmail_com/revox/docker-compose-prod.yml down
+sudo docker-compose -f /home/rikutoweb5/revox/docker-compose-prod.yml down
 
 # 新しいコンテナを起動
-sudo docker-compose -f /home/rikutoweb5_gmail_com/revox/docker-compose-prod.yml up -d
+sudo docker-compose -f /home/rikutoweb5/revox/docker-compose-prod.yml up -d
