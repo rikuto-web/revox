02fe5c82ff7015b6e697622a42707a69d5f1f14f
Shinya <rikutoweb5@gmail.com>
2025-08-27 15:35:52 +0900

    CICDテスト　ログで確認

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ WORKDIR /app
 COPY --from=builder /app/build/libs/*.jar app.jar
 
 # Shell 経由で起動 + プロファイル指定
-ENTRYPOINT ["sh", "-c", "sleep 30 && java -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE} -jar app.jar"]
+ENTRYPOINT ["sh", "-c", "java -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar app.jar"]
--- src/main/java/com/rikuto/revox/RevoxApplication.java
+++ src/main/java/com/rikuto/revox/RevoxApplication.java
@@ -2,12 +2,31 @@ package com.rikuto.revox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RevoxApplication {
+
+	@Value("${DB_USER:undefined}")
+	private String dbUser;
+
+	@Value("${DB_PASSWORD:undefined}")
+	private String dbPassword;
+
+	@Value("${PROD_DB_URL:undefined}")
+	private String prodDbUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevoxApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logEnv() {
+		System.out.println("===== ENV DEBUG =====");
+		System.out.println("DB_USER = " + dbUser);
+		System.out.println("DB_PASSWORD = " + (dbPassword.equals("undefined") ? "undefined" : "********"));
+		System.out.println("PROD_DB_URL = " + prodDbUrl);
+		System.out.println("=====================");
+	}
 }
